5d7589d70a177448a51fc639f649b2ab0ecd6a6c
daggi-01 <dhemd1111@gmail.com>
2024-06-24 17:27:45 +0900

    01.core-js

---
+++ server/index.js
@@ -0,0 +1,24 @@
+
+
+
+// const server = require('live-server');
+
+// const params = {
+//   host: 'localhost',
+//   port: 5000,
+//   open: false,
+//   root: './client'
+// }
+
+
+// server.start(params)
+
+console.log(global)
+
+
+
+
+
+
+
+
