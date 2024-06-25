3d46f04e66e3db7fa4ab0d802f3e6868e65a5955
daggi-01 <dhemd1111@gmail.com>
2024-06-25 10:18:16 +0900

    미완성

--- client/lib/index.js
+++ client/lib/index.js
@@ -1,8 +1,7 @@
-// re-export 
+// re-export
 
-
-
-export * from './dom/index.js'
-export * from './utils/index.js'
-export * from './math/index.js'
-export * from './error/index.js'
+export * from './dom/index.js';
+export * from './math/index.js';
+export * from './error/index.js';
+export * from './utils/index.js';
+export * from './animaton/index.js';
--- client/main.js
+++ client/main.js
@@ -1,15 +1,15 @@
-
-import data from "./data/data.js";
-import { 
-  getNode, 
-  getRandom, 
-  insertLast, 
-  clearContents 
-} from './lib/index.js'
-
-
-
-
+import data from './data/data.js';
+import {
+  shake,
+  getNode,
+  addClass,
+  showAlert,
+  getRandom,
+  insertLast,
+  removeClass,
+  clearContents,
+  isNumericString,
+} from './lib/index.js';
 
 // [phase-1]
 // 1. 주접 떨기 버튼을 클릭 하는 함수
@@ -25,25 +25,48 @@ import {
 
 // 4. pick 항목 랜더링하기
 
+// [phase-2]
+// 1. 아무 값도 입력 받지 못했을 때 예외처리 (콘솔 출력)
 
 const submit = getNode('#submit');
 const nameField = getNode('#nameField');
 const result = getNode('.result');
 
-
-function handleSubmit(e){
+function handleSubmit(e) {
   e.preventDefault();
 
   const name = nameField.value;
   const list = data(name);
   const pick = list[getRandom(list.length)];
 
-  console.log( pick );
-  
-  clearContents(result);
-  insertLast(result,pick);
+  if (!name || name.replace(/\s*/g, '') === '') {
+    showAlert('.alert-error', '공백은 허용하지 않습니다.');
 
-  
+    shake('#nameField').restart();
+
+    return;
+  }
+
+  if (!isNumericString(name)) {
+    showAlert('.alert-error', '제대로된 이름을 입력해 주세요.');
+
+    shake('#nameField').restart();
+
+    return;
+  }
+
+  clearContents(result);
+  insertLast(result, pick);
 }
 
-submit.addEventListener('click',handleSubmit)
+function handleCopy() {
+  const text = result.textContent;
+
+  if (nameField.value) {
+    navigator.clipboard.writeText(text);
+    showAlert('.alert-success', '클립보드 복사 완료!');
+  }
+}
+
+submit.addEventListener('click', handleSubmit);
+result.addEventListener('click', handleCopy);
