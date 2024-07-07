21c3ae316df3cb35813f741722f9744a5bf0d64c
daggi-01 <dhemd1111@gmail.com>
2024-07-07 20:12:14 +0900

    webComponent

--- client/main.js
+++ client/main.js
@@ -1,18 +1,15 @@
-class MyElement extends HTMLElement {
-  constructor() {
-    super();
-  }
-  connectedCollback() {
-    console.log('탄생함');
-  }
-  disconnectedCallback() {
-    console.log('죽음');
-  }
-}
+import { TodoList } from './components/TodoList/TodoList.js';
+import { TodoItem } from './components/TodoItem/TodoItem.js';
 
-customElements.define('c-elemnent', MyElement);
-
-const elem = document.createElement('c-element');
 const app = document.getElementById('app');
 
-app.appendChild(elem);
+const defineElements = () => {
+  customElements.define('todo-app', TodoList);
+  customElements.define('todo-item', TodoItem);
+};
+
+defineElements();
+
+const todoElement = document.createElement('todo-app');
+
+app.append(todoElement);
